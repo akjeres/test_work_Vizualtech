e9ed068702dc199173bc14e98fd0e985917775f6
akjeres <akjeres@gmail.com>
2018-01-25 12:05:24 +0200

    Completed task added

---
+++ js/script.js
@@ -0,0 +1,136 @@
+function space_cut_first (string) {
+	return string.substring(string.indexOf(" ") + 1);
+}
+function space_cut_last (string) {
+	if (string.indexOf(" ") != -1) {
+		return string.substring(0, string.indexOf(" "));
+	}
+	else return string;
+}
+function space_cut (string) {
+	return space_cut_last(space_cut_first(string));
+}
+function prefix_change (prefix, string) {
+	return prefix + string.substring(string.indexOf("_"));
+}
+$("#main_nav li").hover(function() {
+	var nav_selector = $(this).prop("className");
+	if (space_cut(nav_selector).length > 14) {
+		nav_selector = "nav_wedding";
+	} nav_selector = space_cut(nav_selector);
+	if (nav_selector != "nav_wedding") {
+		$(".dropdown.nav_wedding").removeClass("open");
+		$(".ruler_wedding").css({
+			height: "",
+			width: "",
+			backgroundColor: "",
+			marginLeft: ""
+		});
+	}
+	var ruler_selector = prefix_change("ruler", nav_selector);
+	$("." + ruler_selector).css({
+		height: "8px",
+		width: get_wigth(nav_selector),
+		marginLeft: get_marginLeft(nav_selector),
+		backgroundColor: "#1d2759"
+	});
+}, function() {
+	var nav_selector = $(this).prop("className");
+	$("." + prefix_change("ruler", nav_selector)).css({
+		height: "",
+		width: "",
+		backgroundColor: "",
+		marginLeft: ""
+	});
+	//console.log(nav_selector);
+});
+
+function parameter_2_int (string) {
+
+	return parseInt(string.substring(0, string.indexOf("px")));
+}
+function get_wigth(selector) {
+	var w = $("." + selector + " a").css("width");
+	w = parameter_2_int(w);
+	var pl = $("." + selector + " a").css("paddingLeft");
+	pl= parameter_2_int(pl);
+	var pr = $("." + selector + " a").css("paddingRight");
+	pr= parameter_2_int(pr);
+
+	var result = (w-pl-pr)+"px";
+	return result;
+}
+function get_marginLeft (selector) {
+	var wrp = $(".content_wrapper").css("marginLeft");
+	wrp = parameter_2_int(wrp);
+	var offset = $("." + selector).offset().left;
+	var paddingLeft = $("." + selector + " a").css("paddingLeft");
+	paddingLeft = parameter_2_int(paddingLeft);
+	var result = offset + paddingLeft - wrp;
+
+	return result + "px";
+}
+$("#main_nav li").click(function() {
+	if ($(this).hasClass("nav_wedding") && !($(this).hasClass("open")) &&
+		($(window).width() < 768)) {
+		$("#bs-example-navbar-collapse-1").height(512);
+	} else $("#bs-example-navbar-collapse-1").height(365);
+});
+$(document).ready(function() {
+	if ($(window).width() < 1144) {
+		$(".carousel_wrapper").css({
+			position: "static",
+			display: "none"
+	});
+	} else {
+		$(".carousel_wrapper").css({
+			position: "absolute",
+			top: $(".main_title").offset().top - 120,
+			left: ($(".main_article").width() - $(".carousel_wrapper").width()
+				+ $(".main_article").offset().left),
+			display: "block"
+		});
+	}
+	if ($(window).width() < 359) {
+		$(".terms").removeClass("pull-right");
+	}
+	/*Animation Demo Begins*/
+	$(".company_wrapper").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        	top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+    /*Animation Demo Ends*/
+});
+
+$(window).resize(function() {
+	if ($(window).width() < 359) {
+		$(".terms").removeClass("pull-right");
+	} else if (!($(".terms").hasClass("pull-right"))) {
+		$(".terms").addClass("pull-right");
+	}
+	if ($(window).width() < 510) {
+		$("main").height(
+			($(".main_title").height() + parameter_2_int ($(".main_title").css("marginBottom")) +
+			$(".main_article_wrapper").height() + 23) + "px"
+			);
+	} else if ($(window).width() < 1144) {
+		$("main").height("611px");
+	} else $("main").height("791px");
+	if ($(window).width() < 1144) {
+		$(".carousel_wrapper").css({
+			position: "static",
+			display: "none"
+		});
+	} else {
+		$(".carousel_wrapper").css({
+			position: "absolute",
+			top: $(".main_header").offset().top+168,
+			left: ($(".main_article").width() - $(".carousel_wrapper").width()
+				+ $(".main_article").offset().left),
+			display: "block"
+		});
+	}
+	
+});
